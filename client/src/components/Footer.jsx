cf43e99cf4d7a8f710132c736ec849f60a4e11a2
ianderrick <ianderrick341@gmail.com>
2025-09-11 22:40:02 +0300

    updated the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaWhatsapp, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { FaWhatsapp, FaEnvelope, FaMapMarkerAlt, FaFacebookF, FaTiktok, FaInstagram,FaLinkedinIn } from "react-icons/fa";
 
 export default function Footer() {
   return (
@@ -7,21 +7,59 @@ export default function Footer() {
         <div className="mb-4 md:mb-0">
           <h2 className="font-bold text-xl mb-2">Dr. Lydia Mwai</h2>
           <p>Helping SMEs grow through marketing and training.</p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <p className="flex items-center gap-2">
-            <FaWhatsapp /> 
-           <a
+<div className="flex gap-6 mt-6 justify-center md:justify-start text-2xl text-gray-700">
+            <a
+              href="https://www.facebook.com/profile.php?id=61580388701558"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-600 transition"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://instagram.com/lydia.k.mwai/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-pink-500 transition"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://linkedin.com/in/dr-lydia-mwai-phd-mmsk-20495a80/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-700 transition"
+            >
+              <FaLinkedinIn />
+            </a>
+          
+             <a
+              href="https://www.tiktok.com/@pulse.point.marketing?_t=ZP-8zdl4WFSn7P&_r=1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-700 transition"
+            >
+              <FaTiktok />
+            </a>
+
+            <a
               href="https://wa.me/254728840848"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-green-600 hover:underline"
+              className="hover:text-blue-700 transition"
             >
-              +254 728 840 848
+              <FaWhatsapp />
             </a>
-          </p>
+          </div>
+                    
+
+
+        </div>
+        <div className="flex flex-col gap-2">
+         
           <p className="flex items-center gap-2">
-            <FaEnvelope /> lydiakmwai@gmail.com
+            <FaEnvelope />
+            <a href="mailto:lydiakmwai@gmail.com">lydiakmwai@gmail.com</a> 
           </p>
           <p className="flex items-center gap-2">
             <FaMapMarkerAlt /> Kenya (Available Online & In-Person)
